b06aa08e5b3fe00cb0f2b3933f047dc0accbbada
cookiessManis <hafiz.purnama2002@gmail.com>
2024-02-24 23:19:14 +0700

    Stats

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -11,7 +11,7 @@ const GetStarted = () => {
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
       >
         <div className={`${styles.flexStart} flex-row`}>
-          <p className="font-poppins font-medium leading-[23px]">
+          <p className="font-poppins font-medium leading-[23px] mr-2">
             <span className="text-gradient">Get</span>
             <img
               src={arrowUp}
@@ -20,6 +20,9 @@ const GetStarted = () => {
             />
           </p>
         </div>
+        <p className="font-poppins font-medium leading-[23px]">
+          <span className="text-gradient">Started</span>
+        </p>
       </div>
     </div>
   );
