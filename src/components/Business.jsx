a32036f0d4356edf68924c225bef8cb106fa2aa0
cookiessManis <hafiz.purnama2002@gmail.com>
2024-02-25 13:02:10 +0700

    Business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { features } from "../constans";
+import styles, { layout } from "../style";
+import Button from "./Button";
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={`${layout.sectionInfo}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          You do the business, <br className="sm:block hidden" /> we'II handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph}`}>
+          with the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Business;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { stats } from "../constans";
+import styles from "../style";
 
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} flex flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className="flex-1 flex flex-row justify-start m-3 items-center"
+        >
+          <h4
+            className={`text-white font-poppins font-semibold xs:text-[40px] text-[30px] leading-[43px]`}
+          >
+            {stat.value}
+          </h4>
+          <p
+            className={` font-poppins font-semibold xs:text-[20px] text-[15px] leading-[21px] uppercase ml-3 text-gradient`}
+          >
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
